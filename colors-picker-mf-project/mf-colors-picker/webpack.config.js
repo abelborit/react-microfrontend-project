e942876018201b701f47c426c3fa840082088c9e
Abel Borit <abelboritguitton@gmail.com>
2023-08-07 17:27:52 -0500

    [add] wedpack modes in webpack.config

--- colors-picker-mf-project/mf-colors-picker/webpack.config.js
+++ colors-picker-mf-project/mf-colors-picker/webpack.config.js
@@ -2,8 +2,11 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const deps = require("./package.json").dependencies;
+/* (_, argv) son un argumento que se le conoce como guión bajo y un argv y este trae los modos de webpack, es decir, en qué modo está (desarrollo, producción, etc), estos modos son en automático que se le asignan */
 module.exports = (_, argv) => ({
   output: {
+    /* cuando se tenga una ruta de producción se realiza de esta forma, para que cuando estemos en desarrollo apunte al puerto 3001 y cuando esté en producción apunto a su url de producción como tal */
+    // publicPath: argv.mode === "development" ? "http://localhost:3001/" : "ruta de producción" ,
     publicPath: "http://localhost:3001/",
   },
 
