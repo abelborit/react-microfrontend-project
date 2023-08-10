2810b912124722671a8229c0a8b7c69b235b0b7e
Abel Borit <abelboritguitton@gmail.com>
2023-08-09 21:24:12 -0500

    [add] lazy loading

--- apiDBZ-mf-project/host-app/src/App.jsx
+++ apiDBZ-mf-project/host-app/src/App.jsx
@@ -1,14 +1,16 @@
 // Usar este paquete todavía utiliza la implementación de React 17, pero si nos fijamos en el package.json ya usa la versión React 18, por eso nos aparece el siguiente problema: **_Warning: ReactDOM.render is no longer supported in React 18. Use createRoot instead. Until you switch to the new API, your app will behave as if it's running React 17._** Para solucionar esto se tiene que modificar el src/App.jsx a la versión de React 18 para poder usar los hooks y las nuevas implementación de la versión 18
 
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import HomePage from "./pages/HomePage";
-import PersonajesPage from "./pages/PersonajesPage";
-import AboutPage from "./pages/AboutPage";
 import NavbarFederated from "NavbarModuleFederation/NavbarFederated";
+import { Loader } from "./components/Loader";
 // import NotFound from "./pages/NotFound";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const PersonajesPage = lazy(() => import("./pages/PersonajesPage"));
+const AboutPage = lazy(() => import("./pages/AboutPage"));
 
 import "./index.scss";
 
@@ -18,9 +20,32 @@ const App = () => (
 
     <div className="text-center mt-8 text-4xl">
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/personajes" element={<PersonajesPage />} />
-        <Route path="/about" element={<AboutPage />} />
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<Loader />}>
+              <HomePage />
+            </Suspense>
+          }
+        />
+
+        <Route
+          path="/personajes"
+          element={
+            <Suspense fallback={<Loader />}>
+              <PersonajesPage />
+            </Suspense>
+          }
+        />
+
+        <Route
+          path="/about"
+          element={
+            <Suspense fallback={<Loader />}>
+              <AboutPage />
+            </Suspense>
+          }
+        />
 
         {/* <Route path="*" element={<NotFound />} /> */}
         <Route path="*" element={<Navigate to="/" />} />
---
+++ apiDBZ-mf-project/host-app/src/components/Loader.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "./Loader.css";
+
+export const Loader = () => {
+  return (
+    <div className="text-center mt-5">
+      <div className="lds-spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
