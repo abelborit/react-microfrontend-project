5ae67223dd633503d5c670752506fb7cb7da75f6
Abel Borit <abelboritguitton@gmail.com>
2023-08-21 22:39:40 -0500

    [create] redux-mf-project

---
+++ redux-mf-project/host-app/src/App.jsx
@@ -0,0 +1,17 @@
+// Usar este paquete todavía utiliza la implementación de React 17, pero si nos fijamos en el package.json ya usa la versión React 18, por eso nos aparece el siguiente problema: **_Warning: ReactDOM.render is no longer supported in React 18. Use createRoot instead. Until you switch to the new API, your app will behave as if it's running React 17._** Para solucionar esto se tiene que modificar el src/App.jsx a la versión de React 18 para poder usar los hooks y las nuevas implementación de la versión 18
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+
+const App = () => (
+  <div className="container">
+    <div>Name: host-app</div>
+    <div>Framework: react</div>
+    <div>Language: JavaScript</div>
+    <div>CSS: Empty CSS</div>
+  </div>
+);
+
+ReactDOM.createRoot(document.getElementById("app")).render(<App />);
